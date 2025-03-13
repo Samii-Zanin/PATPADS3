6a4ef3a78f96b99d8d3508168787c60718b5fe1a
rauld <rauld@vTx>
2025-03-13 20:04:07 -0300

    BOAS
    Signed-off-by: rauld <rauld@vTx>

--- PATPADS3/src/testeGIT/HelloGit.java
+++ PATPADS3/src/testeGIT/HelloGit.java
@@ -5,6 +5,13 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Clonado com sucesso!!!");
+		System.out.println("Clonado com sucesso!!!");
+
+		System.out.println("Clonado com sucesso!!!");
+
+		System.out.println("Clonado com sucesso!!!");
+
+		
 	}
 
 }
