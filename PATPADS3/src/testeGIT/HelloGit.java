14d186463d1c8422eda418ab55059b666dcdc0c5
Samii_Zanin <samiizanin@gmail.com>
2025-03-13 20:19:00 -0300

    teste2

--- PATPADS3/src/testeGIT/HelloGit.java
+++ PATPADS3/src/testeGIT/HelloGit.java
@@ -3,8 +3,10 @@ package testeGIT;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git!");
+		System.out.println("Hello Git!2222222");
 		System.out.println("Clonado com sucesso!!!");
+		System.out.println("Clonado com sucesso!!!");
+		System.out.println("Clonado com sucesso!!!");System.out.println("Clonado com sucesso!!!");System.out.println("Clonado com sucesso!!!");System.out.println("Clonado com sucesso!!!");System.out.println("Clonado com sucesso!!!");
 	}
 
 }
